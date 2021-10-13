bf562c50c1e705b75dfa4ce6901ad92bd1a24955
Olga Abramovich <disole@bk.ru>
2021-10-13 06:45:51 +0500

    first commit

---
+++ src/components/StepCounter.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import StepCounterForm from "./StepCounterForm";
+import StepCounterTable from "./StepCounterTable";
+
+function StepCounter() {
+	const [walks, setWalks] = useState([]);
+
+	const handleAdd = walk => {
+		const newWalks = [...walks];
+		const sameDate = newWalks.find(o => o.date === walk.date)
+
+		if (sameDate) {
+			sameDate.distance += walk.distance
+		} else {
+			newWalks.push(walk);
+			newWalks.sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+		}
+		
+		setWalks(newWalks);
+	}
+
+	const handleDelete = id => {
+			setWalks(prevWalks => prevWalks.filter(o => o.id !== id));
+		}
+
+	return (
+		<div className="walk-counter">
+			<StepCounterForm onAdd={handleAdd} />
+			<StepCounterTable walks={walks} onDelete={handleDelete} />
+		</div>
+	)
+}
+
+export default StepCounter;
